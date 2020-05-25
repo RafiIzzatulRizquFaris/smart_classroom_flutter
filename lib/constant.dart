df234deedf58d86634facd4262516c34791f049f
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-05-26 06:37:31 +0700

    update AttendPresenter and add constant.dart

---
+++ lib/constant.dart
@@ -0,0 +1,3 @@
+class Constant{
+  static String baseUrl = "https://coronaclass.000webhostapp.com/";
+}
--- lib/presenter/AttendPresenter.dart
+++ lib/presenter/AttendPresenter.dart
@@ -1,18 +1,18 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
+import 'package:smartclassroomflutter/constant.dart';
 import 'package:smartclassroomflutter/contract/AttendContract.dart';
 import 'package:smartclassroomflutter/model/AttendModel.dart';
 
 class AttendPresenter implements AttendContractPresenter{
 
   AttendContractView _view;
-
   AttendPresenter(this._view);
 
   @override
   Future<List<DataBean>> getAttendance() async {
-    final _url = "https://coronaclass.000webhostapp.com/absen";
+    final _url = "${Constant.baseUrl}absen";
     Client _client = Client();
     final _response = await _client.get(_url);
     if(_response.statusCode != 200){
@@ -20,7 +20,7 @@ class AttendPresenter implements AttendContractPresenter{
     }
     var content = json.decode(_response.body);
     List list = content['data'];
-    return list.map((e) => DataBean.map(e)).toList();
+    return list.map((e) => DataBean.mapping(e)).toList();
   }
 
   @override
